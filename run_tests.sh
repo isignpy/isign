1e22451275cf28702c6a4ab19e58579c27ac0b89
Neil Kandalgaonkar <neilk@brevity.org>
2019-09-09 08:35:26 -0700

    simplify run_tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,4 @@
 #!/bin/bash -e
-name=$(basename $PWD)
-package=$(echo $name | sed 's/-/_/g')
-
-# look for required apps
-for app in unzip zip; do
-    if ! which $app >/dev/null; then
-        echo "Missing application: $app"
-        exit 1
-    fi
-done
 
 # get env vars for library paths, etc
 source ./env.sh
@@ -18,11 +8,4 @@ source ./env.sh
 
 find . -name '*.pyc' -delete
 
-pushd tests >/dev/null
-version=$(python -c "import $package; print ${package}.__version__")
-echo "Testing $name v${version}"
-
 nosetests
-
-popd >/dev/null
-exit 0
