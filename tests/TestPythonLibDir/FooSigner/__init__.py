41a318c7612796fb8a6300d1a4a38b268b99d877
Neil Kandalgaonkar <neilk@brevity.org>
2019-11-13 16:54:56 -0800

    make FooSigner a subclass

--- tests/TestPythonLibDir/FooSigner/__init__.py
+++ tests/TestPythonLibDir/FooSigner/__init__.py
@@ -1,19 +1,20 @@
 __version__ = '1.0.0'
 
+from isign.signer import Pkcs1Signer
 import logging
 
 log = logging.getLogger(__name__)
 
 
-class FooSigner(object):
+class FooSigner(Pkcs1Signer):
     def __init__(self,
                  keyfile=None,
                  callback=None):
-        self.keyfile = keyfile
+        super(FooSigner, self).__init__(keyfile)
         self.callback = callback
 
     def sign(self, data):
         log.info("I am FooSigning, with this callback...")
         log.info(self.callback)
         self.callback("FooSigner was here")
-        return ''
+        return super(FooSigner, self).sign(data)
