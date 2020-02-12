eaa029fff5fcf84db7f6186ee5a0afd405462172
Steven Hazel <sah@stevenhazel.org>
2020-02-11 18:13:32 -0800

    extract_entitlements was moved to Provisioner

--- tests/test_entitlements.py
+++ tests/test_entitlements.py
@@ -1,5 +1,5 @@
 from isign_base_test import IsignBaseTest
-from isign.bundle import App
+from isign.provisioner import Provisioner
 import logging
 
 log = logging.getLogger(__name__)
@@ -7,7 +7,7 @@ log = logging.getLogger(__name__)
 
 class TestEntitlements(IsignBaseTest):
     def test_entitlements_extraction(self):
-        entitlements = App.extract_entitlements(self.PROVISIONING_PROFILE)
+        entitlements = Provisioner.extract_entitlements(self.PROVISIONING_PROFILE)
         log.debug(entitlements)
         assert entitlements['application-identifier'] == 'ISIGNTESTS.*'
         assert entitlements['get-task-allow'] == True
