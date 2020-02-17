d4952acab63a21c075357c53c80add110af9a383
Neil Kandalgaonkar <neilk@brevity.org>
2020-02-17 15:30:56 -0800

    pep8

--- tests/test_signer_modules.py
+++ tests/test_signer_modules.py
@@ -11,7 +11,6 @@ log = logging.getLogger(__name__)
 class TestSignerModules(IsignBaseTest):
 
     def test_signer_class(self):
-
         calls = []  # Py2 won't allow to refer to outer scope by assignment, e.g. isCalled = True
 
         # To let the outer scope know a thing happened, must alter a mutable object - like an array!
@@ -21,12 +20,12 @@ class TestSignerModules(IsignBaseTest):
 
         output_path = self.get_temp_file()
 
-	isign.resign(
-	    IsignBaseTest.TEST_IPA,
-	    certificate=IsignBaseTest.CERTIFICATE,
-	    provisioning_profiles=[IsignBaseTest.PROVISIONING_PROFILE],
-	    output_path=output_path,
-	    signer_class=CallbackSigner,
-	    signer_arguments={'callback': callback}
-	)
-	assert len(calls) > 0
+        isign.resign(
+            IsignBaseTest.TEST_IPA,
+            certificate=IsignBaseTest.CERTIFICATE,
+            provisioning_profiles=[IsignBaseTest.PROVISIONING_PROFILE],
+            output_path=output_path,
+            signer_class=CallbackSigner,
+            signer_arguments={'callback': callback}
+        )
+        assert len(calls) > 0
